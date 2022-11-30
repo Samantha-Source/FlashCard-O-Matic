c2ba331ed1205dd860c82d86462bf404e3cdad3b
Samantha Brown <S.BrownEMS@gmail.com>
2022-11-30 10:56:13 -0500

    Working Here - all delete handlers for deckId working

--- src/Layout/CardList.js
+++ src/Layout/CardList.js
@@ -17,7 +17,7 @@ export default function CardList(){
         allDecks()
     }, [])
 
-    
+
     const DeleteDeckHandler = (id) => {          
         if(window.confirm("Delete this deck? \n \nYou will not be able to recover it.")) {
             deleteDeck(id);
@@ -28,14 +28,14 @@ export default function CardList(){
 
 
     let listing = deckList.map(({name, description, cards, id}, index) => (
-        <div class="border rounded m-2 p-2" key={index}>
-            <p class="float-right text-muted">{cards.length} cards</p>
-            <h3 class="card-title">{name}</h3>
+        <div className="border rounded m-2 p-2" key={index}>
+            <p className="float-right text-muted">{cards.length} cards</p>
+            <h3 className="card-title">{name}</h3>
             <p>{description}</p>
 
             
             <Link to={`/decks/${id}`}>
-            <button type="button" class="btn btn-secondary">  
+            <button type="button" className="btn btn-secondary">  
             <span className="oi oi-eye"></span> 
             {" "} View 
             </button>
@@ -44,7 +44,7 @@ export default function CardList(){
             {" "}
             
             <Link to={`/decks/${id}/study`}>
-            <button type="button" class="btn btn-primary">
+            <button type="button" className="btn btn-primary">
             <span className="oi oi-book"></span>
             {" "} Study
             </button>
@@ -54,7 +54,7 @@ export default function CardList(){
 
             <button 
             type="button"
-             class="btn btn-danger float-right"
+             className="btn btn-danger float-right"
             onClick={() => {DeleteDeckHandler(id)}} 
               >
             <span className="oi oi-trash"></span>
--- src/Layout/Deck.js
+++ src/Layout/Deck.js
@@ -2,8 +2,8 @@ import React from "react";
 import { useParams, Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { deleteDeck, readDeck } from "../utils/api";
-import DeleteDeckHandler from "./DeleteDeckHandler";
 import { listDecks } from "../utils/api";
+import { useHistory } from "react-router-dom";
 
 
 
@@ -12,7 +12,7 @@ export default function Deck(params){
     const [deckName, setDeckName] = useState("")
     const [deckDescription, setDeckDescription] = useState("")
  
-    
+    const history = useHistory()   
     const {deckId} = useParams();
 
     
@@ -20,21 +20,30 @@ export default function Deck(params){
     useEffect(()=>{
         async function allCards(){
             const response = await readDeck(`${deckId}`);
-            setCards(response.cards)
-            console.log(response.name)
+            setCards(response.cards);
             setDeckName(response.name);
-            setDeckDescription(response.description)
+            setDeckDescription(response.description);
         }
-        allCards()
-    }, [])
+        allCards();
+    }, []);
 
+
+    const DeleteDeckHandler = (deckId) => {          
+        if(window.confirm("Delete this deck? \n \nYou will not be able to recover it.")) {
+            deleteDeck(deckId);
+            history.push("/"); 
+            window.location.reload(false);  
+        }
+    }
 
     
 
-console.log(cards)
+// console.log(cards)
+
+
     //SET THE CARD VIEWS FOR THE PAGE DOWN HERE (IT DOESNT DO THAT RIGHT NOW)
     const theCards = cards.map(({front, back, id}, index )=>(
-        <section key={index} class="border rounded p-2">
+        <section key={index} className="border rounded p-2">
             <p>Front: {front}</p>
             <p>Back: {back}</p>
             <p>id: {id}</p>
@@ -43,15 +52,15 @@ console.log(cards)
 
             <Link to={`/decks/${deckId}/cards/${id}/edit`}>
                 <button type="button" className="btn btn-secondary">
-                <span class="oi oi-pencil"></span> {" "}
+                <span className="oi oi-pencil"></span> {" "}
                 Edit
                 </button>
                 </Link>
                 {" "}
                 {/* <Link to="/"> */}
                 <button type="button"
-                 class="btn btn-danger float-right" 
-                //  onClick={DeleteDeckHandler({deckId})}
+                 className="btn btn-danger float-right" 
+                
                  >
             <span className="oi oi-trash"></span>
             {" "} Delete
@@ -66,9 +75,9 @@ console.log(cards)
         <>
         
             <nav aria-label="breadcrumb">
-                <ol class="breadcrumb">
-                    <li class="breadcrumb-item"><Link to="/">Home</Link></li>
-                    <li class="breadcrumb-item active" aria-current="page">{deckName}</li>
+                <ol className="breadcrumb">
+                    <li className="breadcrumb-item"><Link to="/">Home</Link></li>
+                    <li className="breadcrumb-item active" aria-current="page">{deckName}</li>
                 </ol>
             </nav>
 
@@ -77,34 +86,39 @@ console.log(cards)
                 <p>{deckDescription}</p>
 
                 <Link to={`/decks/${deckId}/edit`}>
-                <button type="button" class="btn btn-secondary">
+                <button type="button" className="btn btn-secondary">
                 <span className="oi oi-pencil"></span>
                 {" "} Edit
                 </button>
                 </Link>
                 {" "}
                 <Link to={`/decks/${deckId}/study`}>
-                <button type="button" class="btn btn-primary">
+                <button type="button" className="btn btn-primary">
                 <span className="oi oi-book"></span>
                 {" "} Study
                 </button>
                 </Link>
                 {" "}
                 <Link to={`/decks/${deckId}/cards/new`}>
-                <button type="button" class="btn btn-primary">
+                <button type="button" className="btn btn-primary">
                 <span className="oi oi-plus"></span>
                 {" "} Add Cards
                 </button>
                 </Link>
                 {" "}
-                <Link to="/">
+                
+                
                 <button 
                 type="button" 
                 className="btn btn-danger float-right" 
+                onClick={() => {DeleteDeckHandler(deckId)}}
+                ><span className = "oi oi-trash"></span>
+                Delete</button>
                 
-                >
-                Delete(Right now goes to new)</button>
-                </Link>
+
+        
+
+
 
             </section>
             <br></br>
