68b9801b93e05a58ac01f50deabd567849ef5964
Samantha Brown <S.BrownEMS@gmail.com>
2022-11-30 15:18:00 -0500

    LessThan3Study

--- src/Layout/Study.js
+++ src/Layout/Study.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link, useRouteMatch, useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { readDeck } from "../utils/api";
+import { useHistory } from "react-router-dom";
 
 
 
@@ -10,9 +11,10 @@ export default function Study(params){
     const [deckName, setDeckName] = useState("")
     
     const {deckId} = useParams();
+    const history = useHistory();
     
 
-
+    // Read Deck
     useEffect(()=>{
         async function allCards(){
             const response = await readDeck(`${deckId}`);
@@ -22,15 +24,20 @@ export default function Study(params){
         allCards()
     }, [])
     
-    const theCards = cards.map(({front, back}, index )=>(
-        <section key={index}>
-            <p>Front: {front}</p>
-            <p>Back: {back}</p>
+    //Map the cards
+    const theCards = cards.map((card, index )=>(
+        <section key={index} className="border rounded">
+            <p>Card {index+1} of {cards.length}</p>
+            <p>Front: {card.front}</p>
+            <p>Back: {card.back}</p>
         </section>
     ))
-    
-    return(
-        <React.Fragment>
+
+    //Show this if less than 3 cards
+    if(cards.length < 3){
+        return (
+            <React.Fragment>
+            {/* BREADCRUMB NAV BAR */}
         <nav aria-label="breadcrumb">
             <ol className="breadcrumb">
                 <li className="breadcrumb-item"><Link to="/">Home</Link></li>
@@ -38,7 +45,36 @@ export default function Study(params){
                 <li className="breadcrumb-item active" aria-current="page">Study</li>
             </ol>
         </nav>
+        <h3>{deckName}: Study</h3>
+        <h4>Not enough cards.</h4>
+        <p>You need at least 3 cards to study.  There are {cards.length} cards in this deck.</p>
+        <button type="button" className="btn btn-primary" onClick={(()=>{history.push(`/decks/${deckId}/cards/new`)})}>
+            <span className="oi oi-plus"></span>
+            {/* <Link to={`/decks/${deckId}/cards/new`}
+            Add Cards></Link> */}
+            {" "}Add Cards
+        </button>
+        </React.Fragment>
+        )
+    }
+    
 
+
+
+    
+    return(
+        <React.Fragment>
+            {/* BREADCRUMB NAV BAR */}
+        <nav aria-label="breadcrumb">
+            <ol className="breadcrumb">
+                <li className="breadcrumb-item"><Link to="/">Home</Link></li>
+                <li className="breadcrumb-item"><Link to={`/decks/${deckId}`}>{deckName}</Link></li>
+                <li className="breadcrumb-item active" aria-current="page">Study</li>
+            </ol>
+        </nav>
+        <h3>Study: {deckName}</h3>
+
+        {/* THE CARDS  */}
         <ul>
             {theCards}
         </ul>
